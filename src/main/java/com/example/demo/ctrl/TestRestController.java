1fa3eb17277fba1e2995fa06ed47168bfa474803
speed0dev <speed0dev@gmail.com>
2021-05-24 02:06:29 +0900

    add

---
+++ src/main/java/com/example/demo/ctrl/TestRestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiImplicitParam;
+
+@RestController
+public class TestRestController {
+
+	
+	@ApiImplicitParam(name="name", value="이름", required=true)
+	@GetMapping("/greet")
+	public Object getList() {
+		
+		
+		return "AAAAAAAAA";
+	}
+}
